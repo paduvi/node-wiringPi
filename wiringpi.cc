23d1f60c184f2f3ee2036e51cf9bcbdccaa2233d
Phan Đức Việt <viet@techmaster.vn>
2017-06-18 21:12:52 +0700

    bo check num port o pwm write

--- wiringpi.cc
+++ wiringpi.cc
@@ -83,10 +83,10 @@ void PWM_Write(const FunctionCallbackInfo<Value>& args){
         return;
     }
 
-    if( ( args[0]->NumberValue() < 0 ) || ( args[0]->NumberValue() >= NUM_PINS ) ) {
-        isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Index out of bounds")));
-        return;
-    }
+//    if( ( args[0]->NumberValue() < 0 ) || ( args[0]->NumberValue() >= NUM_PINS ) ) {
+//        isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Index out of bounds")));
+//        return;
+//    }
 
     pwmWrite( args[0]->NumberValue(), args[1]->NumberValue() );
 }
